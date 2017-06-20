560ec78ddd2f210d17c4c5c195d1325271cc1ae3
Renato Marteleto <rmarteleto@hotmail.com>
2017-06-19 20:24:31 -0700

    Update solution with clean API solution and PatientAPI

---
+++ src/Controllers/PatientsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MUS3.PatientElectronicAccess.API.Controllers
+{
+    [Route("api/patient")]
+    public class PatientsController: Controller
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            // to-do: need to filter patient from database by id
+            var patient = new Models.Patient();
+            if (patient == null)
+                return NotFound();
+
+            return Ok(patient);
+        }
+    }
+}
---
+++ src/Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿namespace MUS3.PatientElectronicAccess.API.Models
+{
+    public class Patient
+    {
+        /// <summary>
+        /// to-do: this needs to be fixed
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// to-do: this needs to be fixed
+        /// </summary>
+        public string Name { get; set; }
+
+        public GenderEnum Gender { get; set; }
+    }
+}
