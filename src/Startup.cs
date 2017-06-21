34c734eaad5b5dafbb8b719169e1e94c784c64b6
Renato Marteleto <rmarteleto@hotmail.com>
2017-06-20 20:34:57 -0700

    Simplify xml response support

--- src/Startup.cs
+++ src/Startup.cs
@@ -12,12 +12,7 @@ namespace MUS3.PatientElectronicAccess.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(o =>
-            {
-                o.RespectBrowserAcceptHeader = true;
-                o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
-            });
-
+            services.AddMvc().AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
